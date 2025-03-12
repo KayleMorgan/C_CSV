d1c89824f10f88e79868adb9301b86b39de35e2c
de7m7morgan7 <lenovouser26022015@gmail.com>
2025-03-12 11:23:53 +0200

    Добавьте файлы проекта.

---
+++ C_15_CSV/Source.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE 1024  
+
+typedef struct {
+    int id;
+    char name[20];
+    float salary;
+} Employees;
+
+int main() {
+    FILE* csv;
+    char line[MAX_LINE];
+
+    errno_t err = fopen_s(&csv, "employees.csv", "r");
+    if (err != 0 || !csv) {
+        perror("Ошибка открытия файла");
+        return 1;
+    }
+
+    while (fgets(line, sizeof(line), csv)) {
+        char* context;
+        char* token = strtok_s(line, ";", &context);
+        Employees emp;
+
+        while (token) {
+            if (token) emp.id = atoi(token);
+
+            token = strtok_s(NULL, ";", &context);
+            if (token) strcpy_s(emp.name, sizeof(emp.name), token);
+
+            token = strtok_s(NULL, ";", &context);
+            if (token) emp.salary = atof(token);
+        }
+        printf("ID: %d, Name:%s, Salary: %.2f\n", emp.id, emp.name, emp.salary);
+    }
+    fclose(csv);
+}
